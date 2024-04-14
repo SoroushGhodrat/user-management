82b632a6a0eed8f678a7a9be932a80a34546e3b0
Soroush <s.ghodrat@gmail.com>
2024-04-14 19:12:12 +0200

    initial

--- src/store.tsx
+++ src/store.tsx
@@ -7,20 +7,9 @@ export const store = configureStore({
   },
 });
 
-console.log("STORE DATA: ", store.getState());
+// console.log("STORE DATA: ", store.getState());
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-// store.ts
-// import { configureStore } from '@reduxjs/toolkit';
-// import rootReducer from './rootReducer';
-
-// const store = configureStore({
-//   reducer: rootReducer,
-// });
-
-// export type AppDispatch = typeof store.dispatch;
-// export type RootState = ReturnType<typeof rootReducer>;
-// export default store;

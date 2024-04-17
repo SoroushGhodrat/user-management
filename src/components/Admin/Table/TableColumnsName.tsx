47f48df92aef3ca8accdcdbfbf8ddedfff6681d7
Soroush <s.ghodrat@gmail.com>
2024-04-17 09:05:52 +0200

    TableColumnsName component

---
+++ src/components/Admin/Table/TableColumnsName.tsx
@@ -0,0 +1,26 @@
+import { Checkbox } from "@mui/joy";
+
+type Headers = string | JSX.Element;
+
+const TableColumnsName = () => {
+  const headers: Headers[] = [
+    <Checkbox variant="outlined" size="sm" />,
+    "Name",
+    "Role",
+    "Email",
+    "Created on",
+    "Phone",
+    "Status",
+    "",
+  ];
+
+  return (
+    <tr>
+      {headers.map((header, index) => (
+        <th key={index}>{header}</th>
+      ))}
+    </tr>
+  );
+};
+
+export default TableColumnsName;

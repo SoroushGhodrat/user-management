f5b960e6d312f265c1d6d1dbd52bd0d867fa183a
Soroush <s.ghodrat@gmail.com>
2024-04-25 17:28:38 +0200

    Added arrow for sorting

--- src/components/Admin/Table/TableColumnsName.tsx
+++ src/components/Admin/Table/TableColumnsName.tsx
@@ -1,10 +1,15 @@
-import { Checkbox } from '@mui/joy'
+import { TabName } from '@/models/user'
+import { Box } from '@mui/joy'
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
 
 type Headers = string | JSX.Element
 
-const TableColumnsName = () => {
-  const headers: Headers[] = [
-    <Checkbox variant='outlined' size='sm' />,
+const TableColumnNames = () => {
+  const tabName = (localStorage.getItem('tab_name') as TabName) || 'users'
+
+  const userColumnNames: Headers[] = [
+    // <Checkbox variant='outlined' size='sm' label='' />,
+    '',
     'Name',
     'Role',
     'Email',
@@ -14,13 +19,33 @@ const TableColumnsName = () => {
     '',
   ]
 
+  const roleColumnNames: Headers[] = [
+    // <Checkbox variant='outlined' size='sm' label='' />,
+    '',
+    'Role',
+    'Created on',
+    'Status',
+    '',
+  ]
+
+  const headers = tabName === 'users' ? userColumnNames : roleColumnNames
+
+  const arrowBlackList = ['', 'Phone']
+
   return (
     <tr>
       {headers.map((header, index) => (
-        <th key={index}>{header}</th>
+        <th key={index}>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            {header}
+            {typeof header === 'string' && !arrowBlackList.includes(header) && (
+              <ArrowDownwardIcon sx={{ color: 'blue', fontSize: '1rem', pl: 1 }} />
+            )}
+          </Box>
+        </th>
       ))}
     </tr>
   )
 }
 
-export default TableColumnsName
+export default TableColumnNames

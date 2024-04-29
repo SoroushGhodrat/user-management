0d30bebdb09fc3d3102718a576fbec5709b56c2c
Soroush <s.ghodrat@gmail.com>
2024-04-29 09:12:22 +0200

    Code formatting

--- src/components/Admin/UserDeleteModal.tsx
+++ src/components/Admin/UserDeleteModal.tsx
@@ -29,7 +29,7 @@ type UserDeleteModalProps = {
 const UserDeleteModal: React.FC<UserDeleteModalProps> = ({ isOpen, onClose, user }) => {
   const dispatch: AppDispatch = useDispatch()
 
-const userIds = Array.isArray(user) ? user.map((_user: User) => _user.id) : [];
+  const userIds = Array.isArray(user) ? user.map((_user: User) => _user.id) : []
 
   const handleDeleteUser = (userId: string) => {
     if (Array.isArray(user)) {
